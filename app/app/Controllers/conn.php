aac20d0925ad175c07b6d9e9149231f4a19b772f
Syst5246 <87436977+Syst5246@users.noreply.github.com>
2023-03-13 22:08:43 +0200

    123

---
+++ app/app/Controllers/conn.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "id19133048_moonu";
+$password = "Vs~R~u}!7!G)A?vN";
+$dbname = "id19133048_moonn";
+
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+
+if(!$conn) {
+
+die(" PROBLEM WITH CONNECTION : " . mysqli_connect_error());
+
+}
+  
+?>
